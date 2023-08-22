1d78041862684977ba6ff8cda4d676567759d4f2
Guilherme <guilherme@sbsistemas.com.br>
2023-08-22 16:00:19 -0300

    feat: create simple Button and its types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,68 @@
-import { Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
+import { View } from 'react-native'
+import { Button as PaperButton } from 'react-native-paper'
 
 import { ButtonProps } from './Button.types'
 
-const Button = ({ label, onPress }: ButtonProps) => {
+export default function Button({
+  title,
+  icon,
+  materialIconName,
+  alignIcon = 'left',
+  loading = false,
+  onPress,
+  mode = 'contained',
+  ...rest
+}: ButtonProps) {
+  const handleIcon = React.useCallback(() => {
+    if (icon) {
+      return icon
+    }
+    if (materialIconName) {
+      return <MaterialIcons name={materialIconName} size={20} color={'#fff'} />
+    }
+    return undefined
+  }, [icon, materialIconName])
+
+  const handleIconGap = React.useCallback((): number => {
+    if (handleIcon() && title) {
+      return 12
+    }
+    if (title) {
+      return 0
+    }
+    if (handleIcon()) {
+      return -12
+    }
+    return 0
+  }, [handleIcon, title])
+
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>{label}</Text>
-    </TouchableOpacity>
+    <View style={{ paddingHorizontal: 20, width: '100%' }}>
+      <PaperButton
+        disabled={loading}
+        {...rest}
+        icon={handleIcon}
+        mode={mode}
+        onPress={onPress}
+        loading={loading}
+        uppercase
+        style={{
+          borderRadius: 4,
+        }}
+        labelStyle={{
+          fontSize: 14,
+          fontWeight: '700',
+        }}
+        contentStyle={{
+          gap: handleIconGap(),
+          flexDirection: alignIcon === 'right' ? 'row-reverse' : 'row',
+          height: 60,
+        }}
+      >
+        {title}
+      </PaperButton>
+    </View>
   )
 }
-
-export default Button
--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,4 +1,14 @@
-export interface ButtonProps {
-  label: string;
-  onPress: () => void
+import { MaterialIcons } from '@expo/vector-icons'
+import {
+  ButtonProps as PaperButtonProps,
+} from 'react-native-paper'
+
+export interface ButtonProps
+  extends Omit<PaperButtonProps, 'icon' | 'children'> {
+  title?: string | React.ReactNode
+  onPress?: () => void
+  materialIconName?: keyof typeof MaterialIcons.glyphMap
+  icon?: React.ReactNode
+  alignIcon?: 'left' | 'right'
+  loading?: boolean
 }
