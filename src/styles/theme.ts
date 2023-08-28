78a5685042d592519fa52ddef1848b34964475e2
guilherme_gbm <guiwxz@hotmail.com>
2023-08-28 16:52:37 -0300

    refactor: change TODO commentary

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,47 +3,8 @@ import {
   MD3Theme,
 } from 'react-native-paper'
 
-// const colors = {
-//   primary: {
-//       100: '#E56B2E',
-//       150: '#1E2D32'
 
-//   },
-//   beige: {
-//       100: '#ECE8DE'
-//   },
-//   secondary: {
-//       100: '#1D1D1D',
-//   },
-//   gray: {
-//       100: '#1D1D1DCC',
-//       200: '#1D1D1D99',
-//       300: '#1D1D1D1A',
-//   },
-//   lightBlue: {
-//       100: '#F4F5F9',
-//   },
-//   yellow: {
-//       100: '#F1C40F',
-//   },
-//   white: {
-//       100: '#ffffff'
-//   },
-//   grey: {
-//       100: '#656669',
-//       150: '#455A64',
-//       200: '#263238'
-//   },
-//   orange: {
-//       100:'#E56B2E'
-//   },
-//   green: {
-//       100: '#34A100'
-//   }
-// }
-
-
-//TODO interessante personalizar theme para manter padrão nos componentes
+//TODO: WEB3-51 - use Bioca colors as theme
 export const theme: MD3Theme = {
   ...DefaultTheme,
   colors: {
